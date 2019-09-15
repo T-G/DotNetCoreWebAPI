f85cbf0662f03a2800002603a1ce75e8d63763bb
T-G <pronoygomes@yahoo.com>
2019-09-15 16:26:25 +0100

    replaced .net standard class project wit .netcore 2.2 project

---
+++ OrgDAL/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OrgDAL
+{
+    public class Department
+    {
+
+        [Required]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int DId { get; set; }
+        [Required]
+        public string DName { get; set; }
+        public string Description { get; set; }
+    }
+}
--- OrgDAL/OrganizationDbContext.cs
+++ OrgDAL/OrganizationDbContext.cs
@@ -3,12 +3,14 @@ using System;
 
 namespace OrgDAL
 {
-    public class OrganizationDbContext : DbContext
+    public class OrganizationaDbContext: DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseSqlServer(@"Server=THINKPADX220\SQLEXPRESS;Database=OrgAPIDb;Trusted_Connection=True;MultipleActiveResultSets=true");
         }
+
+        public DbSet<Department> Departments { get; set; }
     }
 }
