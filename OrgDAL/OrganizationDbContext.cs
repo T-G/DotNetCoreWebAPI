4fb80c88c8586e482e0c01af3a16703796d0b903
T-G <pronoygomes@yahoo.com>
2019-09-15 12:34:52 +0100

    Initial Commit created Database connection

---
+++ OrgDAL/OrganizationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace OrgDAL
+{
+    public class OrganizationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=THINKPADX220\SQLEXPRESS;Database=OrgAPIDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
